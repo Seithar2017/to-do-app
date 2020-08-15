02932ee33704d25c63439ca1d89a93026e182a1a
Krzysztof Waszkiewicz <kwaszkiewicz94@gmail.com>
2020-08-15 13:28:30 +0200

    Updates

---
+++ precache-manifest.32abd830168728b12a6220370e52578f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b91848d53d10aac1786d",
+    "url": "/to-do-app/static/css/main.1b43f853.chunk.css"
+  },
+  {
+    "revision": "b91848d53d10aac1786d",
+    "url": "/to-do-app/static/js/main.b91848d5.chunk.js"
+  },
+  {
+    "revision": "f6b84cc0c44b8101ab3f",
+    "url": "/to-do-app/static/js/1.f6b84cc0.chunk.js"
+  },
+  {
+    "revision": "60e6816a297c28fee7c8",
+    "url": "/to-do-app/static/js/runtime~main.60e6816a.js"
+  },
+  {
+    "revision": "8addf91cdac3702a2fecd01da5af66d7",
+    "url": "/to-do-app/index.html"
+  }
+];
